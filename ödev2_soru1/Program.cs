b8df16174b75e91c09b178432450ae574d55ff9e
Your Name <your.email@example.com>
2024-09-06 14:42:56 +0300

    kelime_icinden_harf_degistiren_program
    nesne tabanlı programlama ödevim olan consolda kelime içinden harf değiştiren program

---
+++ ödev2_soru1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev2_soru1
+{
+    class Program
+    { 
+        static string soru1cozum( string cumle1)
+        {
+            char harf, yeniHarf;
+            Console.WriteLine("Değiştirilecek harfi giriniz:");
+            harf = char.Parse(Console.ReadLine());
+            Console.WriteLine("Harfin yeni değerini giriniz:");
+            yeniHarf = char.Parse(Console.ReadLine());
+            for (int i = 0; i < cumle1.Length; i++)
+            {
+                if (cumle1[i] == harf)
+                {
+                    cumle1 = cumle1.Replace(harf, yeniHarf);
+
+                }
+
+            }
+            return cumle1;
+        }
+        static void Main(string[] args)
+        {
+            String cumle = "";
+            Console.Write("Cümleyi giriniz: ");
+            cumle = Console.ReadLine();
+            cumle=soru1cozum(cumle);
+            Console.WriteLine(" " + cumle);
+            Console.ReadKey();
+
+
+        }
+    }
+}
